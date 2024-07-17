ee9ce44f63e5dcc9e775804273b3ee8bdabd9696
mbrgr <max.j.ber96@gmail.com>
2024-07-17 16:35:46 +0200

    Weather Data
    All figures are reproducable and labeled

--- weather_data.R
+++ weather_data.R
@@ -30,6 +30,10 @@ load("data/weather_data_raw.RData")
 N$DATUM = str_split_i(N$MESS_DATUM, pattern = " ", i = 1) |> ymd()
 
 tage = c(1, 4, 8, 12, 15, 18, 22, 25, 29)
+N_full = N[-(1:6),]
+N = N %>% 
+  filter(TAG %in% tage)
+
 
 N |>  
   filter(TAG %in% c(1, 8, 15, 22, 29)) |> 
@@ -37,6 +41,7 @@ N |>
   geom_line(aes(x = UHRZEIT, y = TT_10, group = JAHR*TAG, colour = JAHR), alpha = .4) +
   facet_wrap(MONAT ~.)
 
+# Figure 9b: Temp. curves in August
 N |>  
   filter(TAG %in% c(1, 15, 29), 
          MONAT == 8) |> 
@@ -54,7 +59,7 @@ N_wide = N |>
 
 #### Plot empirical covariance for consecutive days vs. non consecutive #####
 
-Y_all = N[-(1:6),] |> # exclude first 6 entries since the days is incomplete
+Y_all = N_full |> 
   mutate(UHRZEIT = as.character(UHRZEIT)) |> 
   select(JAHR, MONAT, TAG, UHRZEIT, TT_10) |> 
   pivot_wider(names_from = UHRZEIT, values_from = TT_10) |> 
@@ -107,12 +112,10 @@ p.eval = 72
 W = local_polynomial_weights(144, 0.2, p.eval, T, m = 1)
 Wh05 = local_polynomial_weights(144, 0.5, p.eval, T, m = 1)
 Wh01 = local_polynomial_weights(144, 0.1, p.eval, T, m = 1)
-g_hat = eval_weights(W, Z)
 eval_time = N$UHRZEIT[1:146][seq(2, 144, 2)]
 
-###### Januar ######
-
-# temp curves
+###### January ######
+# Figure 9a: temp curves in January
 N |>  filter(TAG %in% c(1, 15, 29), 
              MONAT == 1) |> 
   ggplot() +
@@ -132,22 +135,11 @@ var_est1 |>
   labs(y = NULL, x = "hour", title = "Std. deviation of temperatur in January") + 
   theme(text = element_text(size = 18)) 
 
-# both std. deviations in one picture
-ggplot() + 
-  geom_line(data    = var_est1, 
-            mapping = aes(x = x, y = sqrt(var_hat1), lty = "January")) + 
-  geom_line(data    = var_est, 
-            mapping = aes(x = x, y = sqrt(var_hat), lty = "August")) + 
-  geom_line(size = .6) + 
-  lims(y = c(0.2, 5)) + 
-  labs(y = NULL, x = "hour") + 
-  theme(text = element_text(size = 18), legend.position = "top") +
-  scale_linetype_discrete(name = "Std. deviation in month:  ") 
-
 
 cov_est_df1 = data.frame(x = W$x.eval, y = W$x.eval, z = g_hat1)
 cs2 = list(c(0, 1), c("lightblue", "darkred"))
 
+# Plot of Covariance estimation: Not in Paper
 plot_ly(cov_est_df1, x = ~x, y = ~y, z = ~g_hat1, size = .4) |> 
   add_surface(colorscale = cs2, alpha = .3) |> 
   layout(scene = list(xaxis = list(title = ""), 
@@ -157,7 +149,7 @@ plot_ly(cov_est_df1, x = ~x, y = ~y, z = ~g_hat1, size = .4) |>
 temp = matrix(diag(g_hat1), p.eval, p.eval)
 cor_hat1 = g_hat1 / sqrt( temp * t(temp) )
 
-
+# Figure 11a
 plot_ly(cov_est_df1, x = ~x*24, y = ~y*24, z = ~cor_hat1, size = .4) |> 
   add_surface(colorscale = cs2, alpha = .3) |> 
   layout(scene = list(xaxis = list(title = ""), 
@@ -167,7 +159,8 @@ plot_ly(cov_est_df1, x = ~x*24, y = ~y*24, z = ~cor_hat1, size = .4) |>
 
 
 ###### August ######
-#est8 = 
+g_hat = cov_estimation(8)
+var_hat = diag(g_hat)
 var_est = tibble(var_hat, x = eval_time)
 var_est |> 
   ggplot(aes(x = x, y = sqrt(var_hat))) + 
@@ -180,6 +173,7 @@ var_est |>
 cov_est_df = data.frame(x = W$x.eval, y = W$x.eval, z = g_hat)
 cs2 = list(c(0, 1), c("lightblue", "darkred"))
 
+# Plot of Covariance Kernel: Not in Paper
 plot_ly(cov_est_df, x = ~x, y = ~y, z = ~g_hat, size = .4) |> 
   add_surface(colorscale = cs2, alpha = .3) |> 
   layout(scene = list(xaxis = list(title = ""), 
@@ -189,81 +183,14 @@ plot_ly(cov_est_df, x = ~x, y = ~y, z = ~g_hat, size = .4) |>
 temp = matrix(diag(g_hat), p.eval, p.eval)
 cor_hat = g_hat / sqrt( temp * t(temp) )
 
-
+# Figure 11b: Plot of Correlation in August.
 plot_ly(cov_est_df, x = ~x*24, y = ~y*24, z = ~cor_hat, size = .4) |> 
   add_surface(colorscale = cs2, alpha = .3) |> 
   layout(scene = list(xaxis = list(title = ""), 
                       yaxis = list(title = ""), 
                       zaxis = list(title = "")))
 
-
-
-###### April ######
-Y4 = N_wide |> 
-  filter(MONAT == 4) |> 
-  select(-(1:3))
-
-Z4 = Y4 |> 
-  as.data.frame() |> # TODO: fix this in biLocPol
-  observation_transformation(na.rm = T)
-
-N |>  
-  filter(TAG %in% c(1, 15, 29), 
-         MONAT == 1) |> 
-  ggplot() +
-  geom_line(aes(x = UHRZEIT, y = TT_10, group = JAHR*TAG, colour = JAHR), alpha = .7) + 
-  labs(y = "Temp. in C°", x = "hours", title = "Temp. in January", colour = "year") + 
-  theme(text = element_text(size = 18)) 
-
-g_hat4 = eval_weights(W, Z4)
-var_hat4 = diag(g_hat4)
-var_est4 = tibble(var_hat = var_hat4, x = eval_time)
-
-cov_est_df4 = data.frame(x = W$x.eval, y = W$x.eval, z = g_hat4)
-cs2 = list(c(0, 1), c("lightblue", "darkred"))
-
-temp = matrix(diag(g_hat4), p.eval, p.eval)
-cor_hat4 = g_hat4 / sqrt( temp * t(temp) )
-
-
-# all std deviations in one picture
-sd_tibble = rbind(var_est1, 
-                  var_est4, 
-                  var_est, 
-                  var_est10) |> 
-  as_tibble() |> 
-  mutate(var_hat = sqrt(var_hat), 
-         month = gl(4, 72, labels = c("Jan", "Apr", "Aug", "Oct"))) |> 
-  rename(sd = var_hat)
-sd_tibble |> 
-  ggplot(aes(x = x, y = sd, col = month, lty = month)) + 
-  geom_line(linewidth = .7) + 
-  lims(y = c(0.2, 5.8)) + 
-  labs(y = NULL, x = "hour") + 
-  theme(text = element_text(size = 18), legend.position = "top") 
-
-
-ggplot() + 
-  geom_line(data    = var_est1, 
-            mapping = aes(x = x, y = sqrt(var_hat1), lty = "January")) + 
-  geom_line(data    = var_est, 
-            mapping = aes(x = x, y = sqrt(var_hat), lty = "August")) + 
-  geom_line(data    = var_est4, 
-            mapping = aes(x = x, y = sqrt(var_hat4), lty = "April")) + 
-  geom_line(linewidth = .6) + 
-  lims(y = c(0.2, 5.8)) + 
-  labs(y = NULL, x = "hour") + 
-  theme(text = element_text(size = 18), legend.position = "top") +
-  scale_linetype_discrete(name = "Std. deviation in month:  ") 
-
-plot_ly(cov_est_df1, x = ~x*24, y = ~y*24, z = ~cor_hat1, size = .4) |> 
-  add_surface(colorscale = cs2, alpha = .3) |> 
-  layout(scene = list(xaxis = list(title = ""), 
-                      yaxis = list(title = ""), 
-                      zaxis = list(title = "")))
-
-
-###### std_devaition ######
+###### std_deviation ######
 
 sd_tibble_m1h02 = sapply(1:12, 
                          function(m){
@@ -295,6 +222,7 @@ sd_tibble = sd_tibble_m1h01 |>
 
 sd_tibble$time  = sd_tibble$time |> as.POSIXct(format = "%H:%M")
 
+# Figure 10: all standard deviations. 
 sd_tibble |> 
   ggplot(aes(x = time, y = sd, lty = h, col = h)) + 
   geom_line(linewidth = .8) + 
@@ -305,63 +233,3 @@ sd_tibble |>
   scale_linetype_manual(values = c(2,5,4), name = "h (min)") + 
   scale_color_manual(values = 1:3, name = "h (min)") + 
   scale_x_datetime(date_breaks = "8 hours", date_labels = "%H:%M")
-
-##### Luftdruck #####
-
-N_wide_pp = N |> 
-  mutate(UHRZEIT = as.character(UHRZEIT)) |> 
-  select(JAHR, MONAT, TAG, DATUM, UHRZEIT, PP_10) |> 
-  pivot_wider(names_from = UHRZEIT,
-              values_from = PP_10, ) |> 
-  relocate(JAHR, MONAT, TAG, 
-           .before = DATUM)
-
-Y_pp = N_wide_pp |> 
-  filter(MONAT == 8) |> 
-  select(-(1:4))
-
-# mean
-Y_mean = colMeans(Y_pp, na.rm = T)
-Y_mean_est = locpol::locLinSmootherC(0:143/144, Y_mean, 0:143/144, 0.08, EpaK)
-Y_mean_est = data.frame(Y_mean_est, UHRZEIT = N$UHRZEIT[7:150])
-
-
-ggplot() + 
-  geom_line(data    = N |> filter(MONAT == 8, JAHR > 2020), 
-            mapping = aes(x = UHRZEIT, y = PP_10, group = DATUM)) + 
-  geom_line(data    = Y_mean_est, 
-            mapping = aes(x = UHRZEIT, y = beta0), 
-            col     = "red")
-
-
-Z_pp = Y_pp |> 
-  as.data.frame() |> # TODO: fix this in biLocPol
-  observation_transformation(na.rm = T)
-
-
-# h_cv = k_fold_cv(Y, H, K = 5, m = 1, h.parallel = T, h.parallel.environment = T, na.rm = T)
-p.eval = 75
-W_pp = local_polynomial_weights(144, 0.3, p.eval, T)
-g_hat_pp = eval_weights(W_pp, Z_pp)
-
-cov_est_pp = data.frame(x = W$x.eval, y = W$x.eval, z = g_hat_pp)
-cs2 = list(c(0, 1), c("lightblue", "darkred"))
-
-plot_ly(cov_est_df, x = ~24*x, y = ~24*y, z = ~g_hat_pp, size = .4) |> 
-  add_surface(colorscale = cs2, alpha = .3) |> 
-  layout(scene = list(xaxis = list(title = "x"), 
-                      yaxis = list(title = "y"), 
-                      zaxis = list(title = "")))
-
-temp = matrix(diag(g_hat_pp), p.eval, p.eval)
-cor_hat_pp = g_hat_pp / sqrt( temp * t(temp) )
-
-
-plot_ly(cov_est_pp, x = ~x*24, y = ~y*24, z = ~cor_hat_pp, size = .4) |> 
-  add_surface(colorscale = cs2, alpha = .3) |> 
-  layout(scene = list(xaxis = list(title = "x"), 
-                      yaxis = list(title = "y"), 
-                      zaxis = list(title = "")))
-
-#
-
